d05730716f7e4cd9ba9923cd3a3dd1058bf319b4
jmoore <josh@glencoesoftware.com>
2017-05-02 10:57:56 +0200

    Add modify test

--- tests/test_read_only.py
+++ tests/test_read_only.py
@@ -1,8 +1,19 @@
 import testinfra.utils.ansible_runner
 import pytest
+import uuid
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
+
+CMD = 'env PGPASSWORD=%s psql publicdb -h localhost -U %s -c "%s" -At'
+
+
+def run(Command, sql, should_pass, password, name):
+    try:
+        Command.check_output(CMD % (password, name, sql))
+        assert should_pass
+    except:
+        assert not should_pass
 
 
 @pytest.mark.parametrize("name,password,table,should_pass", [
@@ -13,9 +24,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ])
 def test_query(Command, name, password, table, should_pass):
     sql = "SELECT * FROM " + table
-    txt = 'env PGPASSWORD=%s psql publicdb -h localhost -U %s -c "%s" -At'
-    try:
-        Command.check_output(txt % (password, name, sql))
-        assert should_pass
-    except:
-        assert not should_pass
+    run(Command, sql, should_pass, password, name)
+
+
+@pytest.mark.parametrize("name,password,table,should_pass", [
+    ('alice', 'alice123', "regular", True),
+    ('alice', 'alice123', "password", True),
+    ('charles', 'charles123', "regular", False),
+    ('charles', 'charles123', "password", False),
+])
+def test_modify(Command, name, password, table, should_pass):
+    rnd = str(uuid.uuid4())
+    sql = "insert into %s values ('%s')" % (table, rnd)
+    run(Command, sql, should_pass, password, name)
+    sql = "delete from %s" % table
+    run(Command, sql, should_pass, password, name)
