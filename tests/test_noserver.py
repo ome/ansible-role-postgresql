0a7ed08e9cb64505c8229a7d6efcb76de4c6a56b
Sebastien Besson <seb.besson@gmail.com>
2018-11-15 20:02:32 +0000

    Drop Molecule v1 and install new meta-package

--- tests/test_noserver.py
+++
@@ -1,9 +0,0 @@
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('postgresql-noserver')
-
-
-def test_packages(Package):
-    assert Package('postgresql96').is_installed
-    assert not Package('postgresql96-server').is_installed
