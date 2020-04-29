3d575e2a8e4b3ff0b86841ca6642668d636b67e0
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-04-29 11:34:46 +0100

    add support for pg12

--- molecule/default/tests/utils.py
+++ molecule/default/tests/utils.py
@@ -2,7 +2,9 @@ ver_lookup = {
 	"postgresql-95-localhost": "9.5",
 	"postgresql-96-localhost": "9.6",
 	"postgresql-10-localhost": "10",
-	"postgresql-11-localhost": "11"
+	"postgresql-11-localhost": "11",
+    "postgresql-12-localhost": "12"
+
 
 }
 
